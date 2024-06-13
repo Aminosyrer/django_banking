858a90569e7658a13eecd5288e557f32c5d7555c
Aminosyrer <90452342+Amiiiiiiiin@users.noreply.github.com>
2024-06-13 04:11:19 +0200

    .

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'banking'
+    'rest_framework',
+    'banking',
 ]
 
 MIDDLEWARE = [
